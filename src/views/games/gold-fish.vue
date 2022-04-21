b728d89975f8532160662cc0dce20b05ccb58697
shatong <843391622@qq.com>
2022-04-21 19:27:09 +0800

    feat：更新游戏大厅样式

--- src/views/games/gold-fish.vue
+++ src/views/games/gold-fish.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="player-left">
-    <el-card>
-      <span>{{ roomData.leftUser?.username }}</span>
-      <span>{{ roomData.leftUser?.gameStatus }}</span>
-      <el-button v-if="roomData.leftUser?.gameStatus === 1" @click="onReadyClick(0)">取消准备</el-button>
-      <el-button
-        v-if="roomData.leftUser?.gameStatus === 0"
-        type="primary"
-        @click="onReadyClick(1)"
-      >准备</el-button>
-    </el-card>
-  </div>
-  <div class="player-right">
-    <el-card>
-      <span>{{ roomData.rightUser?.username }}</span>
-      <span>{{ roomData.rightUser?.gameStatus }}</span>
-    </el-card>
+  <div class="game-room">
+    <div class="player-left">
+      <el-card>
+        <span>{{ roomData.leftUser?.username }}</span>
+        <span v-if="roomData.leftUser?.gameStatus === 0">等待中</span>
+        <span v-if="roomData.leftUser?.gameStatus === 1">已准备</span>
+        <span v-if="roomData.leftUser?.gameStatus === 2">游戏中</span>
+        <el-button v-if="roomData.leftUser?.gameStatus === 1" @click="onReadyClick(0)">取消准备</el-button>
+        <el-button v-if="roomData.leftUser?.gameStatus === 0" type="primary" @click="onReadyClick(1)">准备</el-button>
+      </el-card>
+    </div>
+    <div class="player-right">
+      <el-card>
+        <span>{{ roomData.rightUser?.username }}</span>
+        <span v-if="roomData.rightUser?.gameStatus === 0">等待中</span>
+        <span v-if="roomData.rightUser?.gameStatus === 1">已准备</span>
+        <span v-if="roomData.rightUser?.gameStatus === 2">游戏中</span>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -99,4 +101,33 @@ socket.on("event", (res: any) => {
 </script>
 
 <style lang="less" scoped>
+.game-room {
+  position: relative;
+  height: 100%;
+
+  .player-left {
+    position: absolute;
+    top: 30%;
+    left: 0;
+  }
+
+  .player-right {
+    position: absolute;
+    top: 30%;
+    right: 0;
+  }
+
+  .el-card {
+    width: 200px;
+    height: 400px;
+
+    :deep(.el-card__body) {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-content: center;
+      justify-content: space-evenly;
+    }
+  }
+}
 </style>
